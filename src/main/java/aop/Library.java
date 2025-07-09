64d11217221a8d009d8e1dba8b51c4f503359e03
AlexandraN <alexandranayanzina@gmail.com>
2025-07-09 16:04:40 -0400

    Adding an LoggingAspect method

---
+++ src/main/java/aop/Library.java
@@ -0,0 +1,13 @@
+package aop;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component("libraryBean")
+public class Library {
+
+  public void getBook(){
+    System.out.println("We are taking a book");
+  }
+}
